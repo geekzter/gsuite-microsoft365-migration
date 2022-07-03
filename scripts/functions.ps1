62d53223e551adc79d39d47c38a5614aae0836c6
Eric van Wijk <eric@van-wijk.com>
2022-07-03 20:08:35 +0200

    Add email addresses for secondary domains

--- scripts/functions.ps1
+++ scripts/functions.ps1
@@ -3,7 +3,8 @@ function Create-MailUser(
     [parameter(Mandatory=$true)][string]$Alias,
     [parameter(Mandatory=$true)][string]$FirstName,
     [parameter(Mandatory=$true)][string]$LastName,
-    [parameter(Mandatory=$true)][string]$PrimaryDomain
+    [parameter(Mandatory=$true)][string]$PrimaryDomain,
+    [parameter(Mandatory=$true)][string[]]$SecondaryDomain
 ) {
     Get-SecurityPrincipal -Filter "Alias -eq '${Alias}'" | Set-Variable existingUser
     if ($existingUser -and ($existingUser.RecipientTypeDetails -ne 'MailUser')) {
@@ -26,7 +27,29 @@ function Create-MailUser(
                      -Password $password
 
         Write-Host "Created Mail user $emailAddress"
-    }                 
+    }
+
+    Get-MailUser -Identity $Alias | Set-Variable mailUser
+
+    $emailAddresses = $mailUser.emailAddresses
+    Write-Debug "`$emailAddresses: $emailAddresses"
+    foreach ($domain in $SecondaryDomain) {
+        $secondaryEmailAddress = "${Alias}@${domain}"
+        if (($secondaryEmailAddress -inotin $emailAddresses) -and ("smtp:${secondaryEmailAddress}" -inotin $emailAddresses)) {
+            $emailAddresses.Add($secondaryEmailAddress) | Out-Null
+            $updateMailUser = $true
+        }
+    }
+
+    if ($updateMailUser) {
+        Write-Debug "`$emailAddresses: $emailAddresses"
+        Write-Verbose "Updating Mail user ${emailAddress}..."
+        Set-MailUser -Identity $Alias -EmailAddresses $emailAddresses
+    }
+
+    if ($DebugPreference -ieq "Continue") {
+        Get-MailUser -Identity $Alias | Sort-Properties | Format-List | Out-String | Write-Debug
+    }
 }
 
 function Login-ExchangeOnline () {
@@ -45,4 +68,10 @@ function Login-ExchangeOnline () {
     if ($login) {
         Connect-ExchangeOnline
     }
+}
+
+function Sort-Properties (
+    [parameter(Mandatory=$true,ValueFromPipeline=$true)][object]$Object
+) {
+    $Object | Select-Object ([string[]]($Object | Get-Member -MemberType Property | %{ $_.Name } | Sort-Object))
 }
