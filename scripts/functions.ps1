c2edc53da89fdf0ff83aac3339e0e7463d2c682b
Eric van Wijk <eric@van-wijk.com>
2022-07-04 20:55:10 +0200

    Create-GoogleMigrationBatch

--- scripts/functions.ps1
+++ scripts/functions.ps1
@@ -1,3 +1,40 @@
+function Create-GoogleMigrationBatch(
+    [parameter(Mandatory=$true)][string[]]$Alias,
+    [parameter(Mandatory=$true)][string]$Domain,
+    [parameter(Mandatory=$false)][string]$DeliveryDomain="office365${Domain}",
+    [parameter(Mandatory=$false)][string]$EndpointName="Gsuite2Office365",
+    [parameter(Mandatory=$false)][string]$CredentialFile=$env:GOOGLE_APPLICATION_CREDENTIALS
+) {
+
+    [System.Collections.ArrayList]$emailAddresses = @()
+    foreach ($a in $Alias) {
+        $emailAddresses.Add("${a}@${Domain}")
+    }
+
+    Write-Debug "Testing migration server availability with email address $($emailAddresses[0])..."
+    Test-MigrationServerAvailability -Gmail -ServiceAccountKeyFileData $([System.IO.File]::ReadAllBytes($CredentialFile)) -EmailAddress $emailAddresses[0]
+    Write-Debug "Migration server availability test successful"
+    
+    Write-Debug "Creating CSV file with batch contents..."
+    $csvFile = New-TemporaryFile
+    Set-Content -Value "EmailAddress" -Path $csvFile
+    foreach ($emailAddress in emailAddresses) {
+        Add-Content -Value $emailAddress -Path $csvFile
+    }
+    Get-Content $csvFile | Out-String | Write-Verbose
+
+    $batchName = $emailAddresses[0]
+
+    Get-MigrationBatch -Identity $batchName -ErrorAction SilentlyContinue | Set-Variable batch
+    if (!$batch) {
+        Write-Debug "Creating batch ${batchName}..."
+        New-MigrationBatch -SourceEndpoint $EndpointName -Name $batchName -CSVData $([System.IO.File]::ReadAllBytes($csvFile)) -TargetDeliveryDomain $DeliveryDomain | Set-Variable batch
+    }
+    $batch | Sort-Properties | Format-List
+
+    Write-Verbose "Starting batch ${batchName}..."
+    Start-MigrationBatch -Identity $batchName | Sort-Properties | Format-List
+}
 
 function Create-MailUser(
     [parameter(Mandatory=$true)][string]$Alias,
@@ -57,6 +94,7 @@ function Login-ExchangeOnline () {
                   | Where-Object State -ieq Opened `
                   | Set-Variable session
 
+    $login = $false
     if ($session) {
         if (-not (Get-OrganizationConfig -ErrorAction SilentlyContinue)) {
             $login = $true
